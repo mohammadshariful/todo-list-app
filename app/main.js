f21f3d6f591af14583ab54763614b7d843c94704
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-02-09 11:45:41 +0600

    todo app project

---
+++ app/main.js
@@ -0,0 +1,52 @@
+"use strict";
+// select all elements
+const inputField = document.querySelector("#text-field");
+const AddButton = document.querySelector("#add-button");
+const tableContainer = document.querySelector(".table-container");
+// declear global variables
+let count = 0;
+// functionalities
+AddButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (inputField.value === "") {
+    alert("Please Enter a string value");
+  } else {
+    count++;
+    //   clear All button visible
+    const showBtn = document.querySelector(".btn-outline-danger");
+    showBtn.style.visibility = "visible";
+    //   create elements
+    const tableRow = document.createElement("tr");
+    tableRow.innerHTML = `
+      <th>${count}</th>
+              <td>${inputField.value}</td>
+              <td>
+                <button class="btn btn-success">Done</button>
+                <button class="btn btn-danger">Delete</button>
+              </td>
+      `;
+
+    tableContainer.appendChild(tableRow);
+    inputField.value = "";
+
+    // done button funtionalites
+    const doneBtn = document.querySelectorAll(".btn-success");
+    for (const buttons of doneBtn) {
+      buttons.addEventListener("click", function (e) {
+        e.target.parentNode.parentNode.style.textDecoration = "line-through";
+      });
+    }
+
+    // delete button funtionalites
+    const deleteBtn = document.querySelectorAll(".btn-danger");
+    for (const buttons of deleteBtn) {
+      buttons.addEventListener("click", function (e) {
+        e.target.parentNode.parentNode.remove();
+      });
+      showBtn.addEventListener("click", function (e) {
+        tableRow.remove();
+      });
+    }
+  }
+});
